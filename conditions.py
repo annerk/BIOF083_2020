48bd92152b4155b00c54df0488f22fb8bc0e50b8
annek <annek@ip-172-31-93-109.ec2.internal>
2020-05-15 18:11:41 +0000

    added hello.py and conditions.py to the repo

---
+++ conditions.py
@@ -0,0 +1,15 @@
+#This program introduces variables, operators, and conditions
+name="Annie"
+age=30
+
+brother="Scott"
+broage=50
+
+agediff=abs(age-broage)
+
+
+print("My name is", name)
+print("My age is", age)
+print("My brother's name is", brother)
+print("My brother's age is", broage)
+print("We were born", agediff, "years apart")
