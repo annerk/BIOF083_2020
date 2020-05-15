4c8855642593fca3e880e347b218272adb9e90f9
annek <annek@ip-172-31-93-109.ec2.internal>
2020-05-15 20:00:04 +0000

    this is the completed RNA seq pipeline script

--- rnaseqpipe.sh
+++ rnaseqpipe.sh
@@ -26,10 +26,23 @@ ls tophat_del/
 cat tophat_wt/align_summary.txt 
 cat tophat_del/align_summary.txt
 
-#index = sorts the data so that the data is handled more efficiently, like     
-samtools index tophat_wt/accepted_hits.bam
+#index = sorts the data so that the data is handled more efficiently, like the index section of a textbook to make things more efficient
+#bam indexing    
+#if samtools doesn't work, run the source line again to refresh where the programs are found
+samtools19 index tophat_wt/accepted_hits.bam
 samtools19 index tophat_del/accepted_hits.bam
 
+#calculate the differential expression between the wt and the del samples
+#-L wt,del this option states to label the wt as first and the del mutant as second so the denominator and the numerator are correctly selected.
+cuffdiff --no-update-check -o cuffdiff_out -L wt,del mm9_chr1.gtf tophat_wt/accepted_hits.bam tophat_del/accepted_hits.bam
+
+#check the cutdiff_out directory to ensure that output files from the previous command are there
+#we want the file gene_exp.diff
+ls -lh cuffdiff_out
+
+
+#run the pipeline
+echo "the RNA seq pipeline is complete"
 
 
 
